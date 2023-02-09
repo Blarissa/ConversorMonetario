3bb49948f563796de333e5cf5bb39bc1c9a1f638
Blarissa <larissabrasil009@gmail.com>
2023-02-08 21:19:29 -0300

    Definindo repositório para desafio2

---
+++ ConversorMonetario.Models/Resultado.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Text.Json.Serialization;
+
+public record class Resultado(
+    [property: JsonPropertyName("info")] Dictionary<string, decimal> Info,
+    [property: JsonPropertyName("result")] decimal ValorConvertido,
+    [property: JsonPropertyName("query")] Hashtable Query
+    )
+{
+    public decimal Taxa = decimal.Round(Info["rate"], 6);
+    public decimal Convertido = decimal.Round(ValorConvertido, 2);
+}
+
+public record class Simbolos([property: JsonPropertyName("symbols")] Hashtable Simbolo) { 
+    public List<string> Permitidos = Simbolo.Keys.Cast<String>().ToList();
+}
